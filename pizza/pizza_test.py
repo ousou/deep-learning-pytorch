0a87ddd1c1f895fd47a32ab7577b0cfd92c80fde
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-10-14 10:27:04 +0300

    Add Luiza's version of pizza_test.py

---
+++ pizza/pizza_test.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+from load_pizza_data import load_pizza_data
+import numpy as np
+
+'''
+STEP 1: LOADING DATASET
+'''
+
+Xtrain, Ytrain, Xtest, Ytest =  load_pizza_data()
+N, dim = np.shape(Xtrain)
+#Ytrain = np.reshape(Ytrain,(-1,1))
+#Ytest = np.reshape(Ytest,(-1,1))
+print np.shape(Ytrain)
+print np.shape(Ytest)
+
+'''
+STEP 2: MAKING DATASET ITERABLE
+'''
+
+train_data = torch.from_numpy(Xtrain)
+train_labels = torch.from_numpy(Ytrain)
+
+test_data = torch.from_numpy(Xtest)
+test_labels = torch.from_numpy(Ytest)
+
+train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
+test_dataset = torch.utils.data.TensorDataset(test_data,test_labels)
+
+batch_size = 100
+n_iters = 10000
+num_epochs = n_iters / (len(train_dataset) / batch_size)
+num_epochs = int(num_epochs)
+print num_epochs
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+'''
+STEP 3: CREATE MODEL CLASS
+'''
+class LogisticRegressionModel(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+    
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+'''
+STEP 4: INSTANTIATE MODEL CLASS
+'''
+
+input_dim = dim
+output_dim = 2
+
+model = LogisticRegressionModel(input_dim, output_dim)
+
+#######################
+#  USE GPU FOR MODEL  #
+#######################
+
+
+#STEP 5: INSTANTIATE LOSS CLASS
+
+criterion = nn.CrossEntropyLoss()
+
+
+#STEP 6: INSTANTIATE OPTIMIZER CLASS
+
+learning_rate = 0.001
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+#STEP 7: TRAIN THE MODEL
+
+
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        #######################
+        #  USE GPU FOR MODEL  #
+        #######################
+        
+        images = Variable(images)
+        labels = Variable(labels)
+        
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+        
+        # Forward pass to get output/logits
+        outputs = model(images)
+        
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+        
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+        
+        # Updating parameters
+        optimizer.step()
+        
+        iter += 1
+        
+        if iter % 500 == 0:
+            # Calculate Accuracy         
+            correct = 0
+            total = 0
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                #######################
+                #  USE GPU FOR MODEL  #
+                #######################
+                images = Variable(images)
+                
+                # Forward pass only to get logits/output
+                outputs = model(images)
+                
+                # Get predictions from the maximum value
+                _, predicted = torch.max(outputs.data, 1)
+                
+                # Total number of labels
+                total += labels.size(0)
+                
+                #######################
+                #  USE GPU FOR MODEL  #
+                #######################
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+            
+            accuracy = 100.0 * correct / total
+            
+            # Print Loss
+            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.data[0], accuracy))
+
