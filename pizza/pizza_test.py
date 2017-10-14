6c59b18a225fcf2c02bcc16f2e6ea0545657f678
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-10-14 10:55:26 +0300

    Add pycache to gitignore

--- pizza/pizza_test.py
+++ pizza/pizza_test.py
@@ -14,8 +14,8 @@ Xtrain, Ytrain, Xtest, Ytest =  load_pizza_data()
 N, dim = np.shape(Xtrain)
 #Ytrain = np.reshape(Ytrain,(-1,1))
 #Ytest = np.reshape(Ytest,(-1,1))
-print(np.shape(Ytrain))
-print(np.shape(Ytest))
+#print(np.shape(Ytrain))
+#print(np.shape(Ytest))
 
 '''
 STEP 2: MAKING DATASET ITERABLE
@@ -34,7 +34,7 @@ batch_size = 100
 n_iters = 10000
 num_epochs = n_iters / (len(train_dataset) / batch_size)
 num_epochs = int(num_epochs)
-print(num_epochs)
+# print(num_epochs)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
                                            batch_size=batch_size, 
@@ -56,13 +56,58 @@ class LogisticRegressionModel(nn.Module):
         out = self.linear(x)
         return out
 
+
+class FeedforwardNeuralNetModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(FeedforwardNeuralNetModel, self).__init__()
+        # Linear function 1: 784 --> 100
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        # Non-linearity 1
+        self.relu1 = nn.ReLU()
+
+        # Linear function 2: 100 --> 100
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        # Non-linearity 2
+        self.relu2 = nn.ReLU()
+
+        # Linear function 3: 100 --> 100
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        # Non-linearity 3
+        self.relu3 = nn.ReLU()
+
+        # Linear function 4 (readout): 100 --> 10
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Linear function 1
+        out = self.fc1(x)
+        # Non-linearity 1
+        out = self.relu1(out)
+
+        # Linear function 2
+        out = self.fc2(out)
+        # Non-linearity 2
+        out = self.relu2(out)
+
+        # Linear function 2
+        out = self.fc3(out)
+        # Non-linearity 2
+        out = self.relu3(out)
+
+        # Linear function 4 (readout)
+        out = self.fc4(out)
+        return out
+
 '''
 STEP 4: INSTANTIATE MODEL CLASS
 '''
 
 input_dim = dim
 output_dim = 2
+hidden_dim = 20
+# hidden_layers_count = 3
 
+#model = FeedforwardNeuralNetModel(input_dim, hidden_dim, output_dim)
 model = LogisticRegressionModel(input_dim, output_dim)
 
 #######################
